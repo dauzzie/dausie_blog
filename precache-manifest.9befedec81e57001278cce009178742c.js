cc338effc6aa0f4554983e83df9eb900813d88ac
Daus <mhusaini@wisc.edu>
2020-08-13 15:42:16 -0500

    Updates

---
+++ precache-manifest.9befedec81e57001278cce009178742c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8a2391cf0b32a84109e0a5d1d2a9af",
+    "url": "/dausie_blog/index.html"
+  },
+  {
+    "revision": "130c104da831f3db0429",
+    "url": "/dausie_blog/static/css/main.0f018143.chunk.css"
+  },
+  {
+    "revision": "4f213d83789172282763",
+    "url": "/dausie_blog/static/js/2.b9b5d32d.chunk.js"
+  },
+  {
+    "revision": "caa12ca1db2e27e89826c68ebd55cc24",
+    "url": "/dausie_blog/static/js/2.b9b5d32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130c104da831f3db0429",
+    "url": "/dausie_blog/static/js/main.8e483396.chunk.js"
+  },
+  {
+    "revision": "7a2056ed1d885c00b868",
+    "url": "/dausie_blog/static/js/runtime-main.c84b4e47.js"
+  },
+  {
+    "revision": "9e93cd65303a7096b7890cafb9d49bca",
+    "url": "/dausie_blog/static/media/favicon.9e93cd65.ico"
+  },
+  {
+    "revision": "173c0a180c76efb50197cf12eb36f75d",
+    "url": "/dausie_blog/static/media/headerbg.173c0a18.jpg"
+  },
+  {
+    "revision": "1c88504160b24d1440049a6152e43bb0",
+    "url": "/dausie_blog/static/media/headerbgdark.1c885041.png"
+  }
+]);
